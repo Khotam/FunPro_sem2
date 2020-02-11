6a97e650745538fcdeff8b9dc83e0cf0ad2e673a
Khotam Bakhromov <00007717@wiut.uz>
2020-02-11 10:38:47 +0500

    tut_17

---
+++ tut_17/WIUT/WIUT/WIUT.DAL/ApplicantList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WIUT.DAL
+{
+    public class ApplicantList
+    {
+        public List<Applicant> GetAllApplicants()
+        {
+            return new ApplicantManager().GetAll();
+        }
+
+        public List<Applicant> SortLinq(ByAttribute attribute)
+        {
+            switch (attribute)
+            {
+                case ByAttribute.Name:
+                    return GetAllApplicants().OrderBy(a => a.Name).ToList();
+                    
+                case ByAttribute.Surname:
+                    return GetAllApplicants().OrderBy(a => a.Surname).ToList();
+
+                case ByAttribute.DoB:
+                    return GetAllApplicants().OrderBy(a => a.DoB).ToList();
+
+                case ByAttribute.Course:
+                    return GetAllApplicants().OrderBy(a => a.Course).ToList();
+                    break;
+            }
+
+            //if we are here - something went wrong
+            return null;
+        }
+
+        public List<Applicant> Search(string value, ByAttribute attribute)
+        {
+            switch (attribute)
+            {
+                case ByAttribute.Name:
+                    return GetAllApplicants().Where(a => a.Name.ToLower().StartsWith(value.ToLower())).ToList();
+                case ByAttribute.Surname:
+                    return GetAllApplicants().Where(a => a.Surname.ToLower().StartsWith(value.ToLower())).ToList();
+            }
+
+            //if we are here - something went wrong
+            return null;
+        }
+
+    }
+}
---
+++ tut_17/WIUT/WIUT/WIUT.DAL/DbManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlServerCe;
+
+namespace WIUT.DAL
+{
+    public class DbManager
+    {
+        protected SqlCeConnection Connection
+        {
+            get
+            {
+                return new SqlCeConnection(Properties.Settings.Default.ConnectionString);
+            }
+        }
+
+    }
+}
